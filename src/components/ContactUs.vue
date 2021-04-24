2110e52d4e940b4d1359d5c1d396f997a4f3275b
scrapasd <47080903+aslam-c@users.noreply.github.com>
2021-04-24 14:04:30 +0530

    contact us change

--- src/components/ContactUs.vue
+++ src/components/ContactUs.vue
@@ -1,34 +1,15 @@
 <template>
-  <div
-    class="h-screen bg-white flex flex-col space-y-10 justify-center items-center"
-  >
-    <div class="bg-white w-100 shadow-xl rounded p-10">
+  <div class="h-screen bg-white flex flex-col justify-center items-center">
+    <div class="bg-white w-200 shadow-xl rounded p-10">
       <h1 class="text-3xl font-medium">Contact Us</h1>
       <p class="text-sm">Reach us by sending a message</p>
 
-      <form class="space-y-5 mt-5">
-        <input
-          type="text"
-          class="w-full h-12 border border-gray-800 rounded px-3"
-          placeholder="Email"
-        />
-        <p class="text-red-900 text-sm">We need a valid Email address</p>
-        <input
-          type="text"
-          class="w-full h-12 border border-gray-800 rounded px-3"
-          placeholder="Phone"
-        />
-        <textarea
-          class="w-full h-12 border border-gray-800 rounded p-2"
-          placeholder="Message"
-        >
-        </textarea>
-        <button
-          class="text-center w-full bg-black rounded-md text-white py-3 font-medium"
-        >
-          Send
-        </button>
-      </form>
+      <div class="">
+        <h3 class="text-2xl font-display">
+          Email:<a href="mailto:info@theqoq.com">info@theqoq.com</a>
+        </h3>
+        Phone:+91 70120068096
+      </div>
     </div>
   </div>
 </template>
